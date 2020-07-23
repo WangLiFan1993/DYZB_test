7140d1e77719d7a0f2423e682b6014b9f4c18de9
王立凡10283393 <wang.lifan@zte.com.cn>
2020-07-23 10:43:13 +0800

    初始化项目

---
+++ DYZB_test/DYZB_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DYZB_test
+//
+//  Created by wanglifan on 2020/7/23.
+//  Copyright © 2020 10283393. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
